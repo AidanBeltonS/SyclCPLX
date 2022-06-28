9f9841e5ca1a370da6dbd02f421f447c9615f83c
aidan.belton <aidan.belton@codeplay.com>
2022-06-28 11:42:33 +0100

    Add no vectorised impl

--- include/sycl_ext_complex.hpp
+++ include/sycl_ext_complex.hpp
@@ -983,8 +983,7 @@ basic_ostream<_CharT, _Traits> &operator<<(basic_ostream<_CharT, _Traits> &__os,
 
 #else // AOS
 
-template <class _v, class _Tp, int _N>
-class complex {
+template <class _v, class _Tp, int _N> class complex {
 private:
   _v re;
   _v im;
@@ -994,6 +993,55 @@ private:
                                                           _Tp &__ac, _Tp &__bd,
                                                           _Tp &__ad, _Tp &__bc,
                                                           _Tp &__x, _Tp &__y) {
+    if (sycl::isnan(__x) && sycl::isnan(__y)) {
+      bool __recalc = false;
+      if (sycl::isinf(__a) || sycl::isinf(__b)) {
+        __a = sycl::copysign(sycl::isinf(__a) ? _Tp(1) : _Tp(0), __a);
+        __b = sycl::copysign(sycl::isinf(__b) ? _Tp(1) : _Tp(0), __b);
+        if (sycl::isnan(__c))
+          __c = sycl::copysign(_Tp(0), __c);
+        if (sycl::isnan(__d))
+          __d = sycl::copysign(_Tp(0), __d);
+        __recalc = true;
+      }
+      if (sycl::isinf(__c) || sycl::isinf(__d)) {
+        __c = sycl::copysign(sycl::isinf(__c) ? _Tp(1) : _Tp(0), __c);
+        __d = sycl::copysign(sycl::isinf(__d) ? _Tp(1) : _Tp(0), __d);
+        if (sycl::isnan(__a))
+          __a = sycl::copysign(_Tp(0), __a);
+        if (sycl::isnan(__b))
+          __b = sycl::copysign(_Tp(0), __b);
+        __recalc = true;
+      }
+      if (!__recalc && (sycl::isinf(__ac) || sycl::isinf(__bd) ||
+                        sycl::isinf(__ad) || sycl::isinf(__bc))) {
+        if (sycl::isnan(__a))
+          __a = sycl::copysign(_Tp(0), __a);
+        if (sycl::isnan(__b))
+          __b = sycl::copysign(_Tp(0), __b);
+        if (sycl::isnan(__c))
+          __c = sycl::copysign(_Tp(0), __c);
+        if (sycl::isnan(__d))
+          __d = sycl::copysign(_Tp(0), __d);
+        __recalc = true;
+      }
+      if (__recalc) {
+        __x = _Tp(INFINITY) * (__a * __c - __b * __d);
+        __y = _Tp(INFINITY) * (__a * __d + __b * __c);
+      }
+    }
+  }
+
+  _SYCL_EXT_CPLX_INLINE_VISIBILITY void static complex_mul_(_Tp &__a, _Tp &__b,
+                                                            _Tp &__c, _Tp &__d,
+                                                            _Tp &__x,
+                                                            _Tp &__y) {
+    _Tp __ac = __a * __c;
+    _Tp __bd = __b * __d;
+    _Tp __ad = __a * __d;
+    _Tp __bc = __b * __c;
+    __x = __ac - __bd;
+    __y = __ad + __bc;
     if (sycl::isnan(__x) && sycl::isnan(__y)) {
       bool __recalc = false;
       if (sycl::isinf(__a) || sycl::isinf(__b)) {
@@ -1065,10 +1113,45 @@ private:
     }
   }
 
+  _SYCL_EXT_CPLX_INLINE_VISIBILITY void static complex_div_(_Tp &__a, _Tp &__b,
+                                                            _Tp &__c, _Tp &__d,
+                                                            _Tp &__x,
+                                                            _Tp &__y) {
+    int __ilogbw = 0;
+    _Tp __logbw = sycl::logb(sycl::fmax(sycl::fabs(__c), sycl::fabs(__d)));
+    if (sycl::isfinite(__logbw)) {
+      __ilogbw = static_cast<int>(__logbw);
+      __c = sycl::ldexp(__c, -__ilogbw);
+      __d = sycl::ldexp(__d, -__ilogbw);
+    }
+    _Tp __denom = __c * __c + __d * __d;
+    __x = sycl::ldexp((__a * __c + __b * __d) / __denom, -__ilogbw);
+    __y = sycl::ldexp((__b * __c - __a * __d) / __denom, -__ilogbw);
+    if (sycl::isnan(__x) && sycl::isnan(__y)) {
+      if ((__denom == _Tp(0)) && (!sycl::isnan(__a) || !sycl::isnan(__b))) {
+        __x = sycl::copysign(_Tp(INFINITY), __c) * __a;
+        __y = sycl::copysign(_Tp(INFINITY), __c) * __b;
+      } else if ((sycl::isinf(__a) || sycl::isinf(__b)) &&
+                 sycl::isfinite(__c) && sycl::isfinite(__d)) {
+        __a = sycl::copysign(sycl::isinf(__a) ? _Tp(1) : _Tp(0), __a);
+        __b = sycl::copysign(sycl::isinf(__b) ? _Tp(1) : _Tp(0), __b);
+        __x = _Tp(INFINITY) * (__a * __c + __b * __d);
+        __y = _Tp(INFINITY) * (__b * __c - __a * __d);
+      } else if (sycl::isinf(__logbw) && __logbw > _Tp(0) &&
+                 sycl::isfinite(__a) && sycl::isfinite(__b)) {
+        __c = sycl::copysign(sycl::isinf(__c) ? _Tp(1) : _Tp(0), __c);
+        __d = sycl::copysign(sycl::isinf(__d) ? _Tp(1) : _Tp(0), __d);
+        __x = _Tp(0) * (__a * __c + __b * __d);
+        __y = _Tp(0) * (__b * __c - __a * __d);
+      }
+    }
+  }
+
 public:
   complex() : re(), im(){};
   complex(_v real, _v imag) : re(real), im(imag){};
-  template <typename T> complex(std::complex<T> c) : re(c.real()), im(c.imag()){};
+  template <typename T>
+  complex(std::complex<T> c) : re(c.real()), im(c.imag()){};
 
   _SYCL_EXT_CPLX_INLINE_VISIBILITY constexpr _v real() const { return re; }
   _SYCL_EXT_CPLX_INLINE_VISIBILITY constexpr _v imag() const { return im; }
@@ -1080,16 +1163,35 @@ public:
 
   friend _SYCL_EXT_CPLX_INLINE_VISIBILITY complex<_v, _Tp, _N>
   operator+(const complex<_v, _Tp, _N> &__x, const complex<_v, _Tp, _N> &__y) {
+#ifdef VECTORIZE
     return complex<_v, _Tp, _N>(__x.re + __y.re, __x.im + __y.im);
+#else
+#pragma unroll
+    complex<_v, _Tp, _N> out;
+    for (size_t i = 0; i < _N; ++i) {
+      out[i].real(__x[i].re + __y[i].re);
+      out[i].imag(__x[i].im + __y[i].im);
+    }
+#endif
   }
 
   friend _SYCL_EXT_CPLX_INLINE_VISIBILITY complex<_v, _Tp, _N>
   operator-(const complex<_v, _Tp, _N> &__x, const complex<_v, _Tp, _N> &__y) {
+#ifdef VECTORIZE
     return complex<_v, _Tp, _N>(__x.re - __y.re, __x.im - __y.im);
+#else
+#pragma unroll
+    complex<_v, _Tp, _N> out;
+    for (size_t i = 0; i < _N; ++i) {
+      out[i].real(__x[i].re + __y[i].re);
+      out[i].imag(__x[i].im + __y[i].im);
+    }
+#endif
   }
 
   friend _SYCL_EXT_CPLX_INLINE_VISIBILITY complex<_v, _Tp, _N>
   operator*(const complex<_v, _Tp, _N> &__z, const complex<_v, _Tp, _N> &__w) {
+#ifdef VECTORIZE
     // vectorisable
     _v __a = __z.real();
     _v __b = __z.imag();
@@ -1109,10 +1211,25 @@ public:
                  __bc[i], __x[i], __y[i]);
 
     return complex<_v, _Tp, _N>(__x, __y);
+#else // VECTORIZE
+
+    _v __a = __z.real();
+    _v __b = __z.imag();
+    _v __c = __w.real();
+    _v __d = __w.imag();
+    _v __x;
+    _v __y;
+#pragma unroll
+    for (size_t i = 0; i < _N; ++i)
+      complex_mul_(__a[i], __b[i], __c[i], __d[i], __x[i], __y[i]);
+
+    return complex<_v, _Tp, _N>(__x, __y);
+#endif // VECTORIZE
   }
 
   friend _SYCL_EXT_CPLX_INLINE_VISIBILITY complex<_v, _Tp, _N>
   operator/(const complex<_v, _Tp, _N> &__z, const complex<_v, _Tp, _N> &__w) {
+#ifdef VECTORIZE
     // vectorisable
     sycl::vec<int, _N> __ilogbw(0);
     _v __a = __z.real();
@@ -1141,6 +1258,19 @@ public:
                 __y[i]);
 
     return complex<_v, _Tp, _N>(__x, __y);
+#else // VECTORIZE
+    _v __a = __z.real();
+    _v __b = __z.imag();
+    _v __c = __w.real();
+    _v __d = __w.imag();
+    _v __x;
+    _v __y;
+#pragma unroll
+    for (size_t i = 0; i < _N; ++i)
+      complex_div_(__a[i], __b[i], __c[i], __d[i], __x[i], __y[i]);
+
+    return complex<_v, _Tp, _N>(__x, __y);
+#endif // VECTORIZE
   }
 };
 
